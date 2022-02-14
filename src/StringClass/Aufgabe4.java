9ebd41ef9bc0fc42304733336a27f6375532bb6e
Carlos-could <jjbbpe@gmail.com>
2022-02-14 16:38:18 +0100

    fechas

---
+++ src/StringClass/Aufgabe4.java
@@ -0,0 +1,9 @@
+package StringClass;
+
+public class Aufgabe4 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
--- src/fechas/fechas.java
+++ src/fechas/fechas.java
@@ -8,26 +8,39 @@ import java.util.Locale;
 
 public class fechas {
     public static void main(String[] args) {
-        //Aufgabe A
-        LocalDate ld = LocalDate.now(); //inmutable
-        System.out.println(ld);
-//Aufgabe B
-        LocalDate lx = LocalDate.of(2000,1,1);
-        System.out.println(lx);
-//Aufgabe C
-        String str = "11-02-2022 10:33:01";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        LocalDateTime dateTime = LocalDateTime.parse(str, formatter);
+//        //Aufgabe A
+//        LocalDate ld = LocalDate.now(); //inmutable
+//        System.out.println(ld);
+//
+//        //Aufgabe B
+//        LocalDate lx = LocalDate.of(2000,1,1);
+//        System.out.println(lx);
+//
+//        //Aufgabe C
+//        String str = "11-02-2022 10:33:01";
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+//        LocalDateTime dateTime1 = LocalDateTime.parse(str, formatter);
+//        LocalDateTime dateTime2 = LocalDateTime.parse("11/02/2022 10:33:01", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+//
+//        System.out.println(dateTime1);
+//        System.out.println(dateTime2);
 
-        LocalDateTime dateTime2 = LocalDateTime.parse("11-02-2022 10:33:01", formatter);
-
-        System.out.println(dateTime);
-        System.out.println(dateTime2);
         //Aufgabe D
 
-        String horaUsa = "11 Februar 2022 7:49";
-        DateTimeFormatter USA = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
-        LocalDateTime dateTimeUsa = LocalDateTime.parse(horaUsa, formatter);
+
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy h:mm a");
+        String strUSA = "11/02/2022 7:49 AM";
+
+        LocalDateTime dateTimeUsa = LocalDateTime.parse(strUSA, formateador);
+        System.out.println(dateTimeUsa);//iso format
+
+        DateTimeFormatter formateador2 = DateTimeFormatter.ofPattern("dd/MM/yyyy h:mm a");
+        String strUSA2 = "11/02/2022 1:49 PM";
+        LocalDateTime fechaLocal = LocalDateTime.parse(strUSA2,formateador2);
+        System.out.println(formateador2.format(fechaLocal));
+
+
+
 
     }
 }
