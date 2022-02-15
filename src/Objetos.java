973ee8f7d3e50f2ee81d8054bed5ab7889e04e42
Carlos-could <jjbbpe@gmail.com>
2022-02-15 23:01:53 +0100

    FERTIG1

---
+++ src/Objetos.java
@@ -0,0 +1,13 @@
+public class Objetos {
+    int edad = 24;
+    String nombre = "JC";
+
+    public void saludar(){
+        System.out.println("Hola" + nombre);
+    }
+
+    public static void main(String[] args) {
+        Objetos ob1 = new Objetos();
+        ob1.saludar();
+    }
+}
