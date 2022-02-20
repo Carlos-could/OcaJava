56f59eed23ad2dc45efb0ed1c4887af878efca35
Carlos-could <jjbbpe@gmail.com>
2022-02-21 00:22:22 +0100

    xdoming

--- src/mull/dummy.java
+++ src/mull/dummy.java
@@ -1,5 +1,6 @@
 package mull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,6 +16,26 @@ public class dummy {
 //        int i=5;
 //        int j = 6;
 //        System.out.print(i < j ? i+" es Menor " + j : i+" es Mayor"+j);
+
+//        ArrayList<Integer> ArrayList;
+        ArrayList <Integer> lista = new ArrayList<>();
+        lista.add(50);
+        lista.add(51);
+        lista.add(52);
+
+
+        System.out.println(lista.get(1));
+        System.out.println(lista.getClass());
+        System.out.println(lista.size());
+        System.out.println(lista.isEmpty());
+        System.out.println(lista.clone());
+        System.out.println(lista.clone());
+
+        for (Integer l:lista){
+            System.out.println(l);
+        }
+
+
 
     }
 }
